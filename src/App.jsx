a0020602a72e2f67f98287bacd47d6efa446debd
Akash Adak <akashadak00023@gmail.com>
2025-07-26 09:49:31 +0530

    add format

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import BulkOrderForm from "./components/BulkOrderForm";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <BulkOrderForm />
+    </div>
+  );
+}
+
+export default App;
